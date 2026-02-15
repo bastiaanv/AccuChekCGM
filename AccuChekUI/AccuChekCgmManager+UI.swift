3f45493e2b3e9ea59233a8dfe21eb2270033fbb6
bastiaanv <bastiankpn7800@gmail.com>
2026-02-15 17:15:46 +0100

    fix: add loop support

--- AccuChekUI/AccuChekCgmManager+UI.swift
+++ AccuChekUI/AccuChekCgmManager+UI.swift
@@ -16,7 +16,7 @@ extension AccuChekCgmManager: CGMManagerUI {
         )
         return .userInteractionRequired(vc)
     }
-
+    
     public func settingsViewController(
         bluetoothProvider _: any LoopKit.BluetoothProvider,
         displayGlucosePreference: LoopKitUI.DisplayGlucosePreference,
@@ -32,11 +32,11 @@ extension AccuChekCgmManager: CGMManagerUI {
     }
 
     public static var onboardingImage: UIImage? {
-        nil
+        UIImage(named: "sensor", in: Bundle(for: AccuChekUIController.self), compatibleWith: nil)
     }
 
     public var smallImage: UIImage? {
-        nil
+        UIImage(named: "sensor", in: Bundle(for: AccuChekUIController.self), compatibleWith: nil)
     }
 
     public var cgmStatusHighlight: (any LoopKit.DeviceStatusHighlight)? {
@@ -44,10 +44,50 @@ extension AccuChekCgmManager: CGMManagerUI {
     }
 
     public var cgmLifecycleProgress: (any LoopKit.DeviceLifecycleProgress)? {
-        nil
+        guard let expiresAt = state.expiresAt, let activatedAt = state.cgmStartTime else {
+            return nil
+        }
+        
+        if expiresAt.addingTimeInterval(.days(-1)) <= Date.now {
+            // Show warning in last 24h
+            return AccuChekKitLifecycleProgress(
+                percentComplete: getCgmProgress(activatedAt: activatedAt),
+                progressState: .warning
+            )
+        }
+        
+        if expiresAt.addingTimeInterval(.days(-3)) <= Date.now {
+            // Show progress in last 72h
+            return AccuChekKitLifecycleProgress(
+                percentComplete: getCgmProgress(activatedAt: activatedAt),
+                progressState: .normalCGM
+            )
+        }
+        
+        return nil
     }
 
     public var cgmStatusBadge: (any LoopKitUI.DeviceStatusBadge)? {
-        nil
+        // Show exclamation mark for every notification (might be sensor failure or calibation required)
+        if !state.cgmStatus.compactMap({ $0.notification }).isEmpty {
+            return AccuChekDeviceStatusBadge()
+        }
+        
+        return nil
     }
+    
+    private func getCgmProgress(activatedAt: Date) -> Double {
+        let age = activatedAt.timeIntervalSinceNow * -1
+        return age / .days(14)
+    }
+}
+
+struct AccuChekDeviceStatusBadge: DeviceStatusBadge {
+    var image: UIImage? = UIImage(systemName: "exclamationmark.triangle")
+    var state: LoopKitUI.DeviceStatusBadgeState = .critical
+}
+
+struct AccuChekKitLifecycleProgress: DeviceLifecycleProgress {
+    var percentComplete: Double
+    var progressState: LoopKit.DeviceLifecycleProgressState
 }
