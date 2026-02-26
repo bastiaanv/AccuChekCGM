43ae94cb3643e3a366b53dddf5aa0d73189d9e56
bastiaanv <bastiankpn7800@gmail.com>
2026-02-26 19:59:42 +0100

    chore: linter

--- AccuChekKitTests/CgmMeasurement.swift
+++ AccuChekKitTests/CgmMeasurement.swift
@@ -7,15 +7,15 @@ class CgmMeasurementTests {
         // bdf6
         let data = Data(hexString: "0d43bdf62c0602fcff5f006d57")
         let measurement = CgmMeasurement(data)
-        
+
         print(measurement.describe)
         #expect(measurement.describe != "")
     }
-    
+
     @Test func testCrash() async throws {
         let data = Data(hexString: "0fe302086801080a40faff5400c492")
         let measurement = CgmMeasurement(data)
-        
+
         print(measurement.describe)
         #expect(measurement.describe != "")
     }
