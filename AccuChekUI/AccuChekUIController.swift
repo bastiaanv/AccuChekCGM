9aaeb65ec3f125e33255bdd6edae256e773c4d57
bastiaanv <bastiankpn7800@gmail.com>
2026-05-23 21:17:17 +0200

    fix: scanning navigation

--- AccuChekUI/AccuChekUIController.swift
+++ AccuChekUI/AccuChekUIController.swift
@@ -197,7 +197,7 @@ class AccuChekUIController: UINavigationController, CGMManagerOnboarding, Comple
                 }
             }
         #else
-            navigateTo(.pairing)
+            navigateTo(.scanning)
         #endif
     }
 }
